7b8c836339c770e1e34679e3ab9ec2278bd001e6
tore <tore.mueller@googlemail.com>
2013-12-14 16:19:53 +0100

    fixed conflict error

--- gg/GGlauncher.py
+++ gg/GGlauncher.py
@@ -10,49 +10,46 @@ import os
 pygame.init()
 
 class GGlauncher():
-    
-    tbimgpath = os.path.join(".", "gg", "data", "gfx", "TestButton.png")
-    
+
+    lbimgpath = os.path.join(".", "gg", "data", "gfx", "LoginButton.png")
+    ebimgpath = os.path.join(".", "gg", "data", "gfx", "ExitButton.png")
+
     def __init__(self):
-        self.launcherScreen = pygame.display.set_mode((300,500), pygame.NOFRAME)
+        self.launcherScreen = pygame.display.set_mode((300, 500), pygame.NOFRAME)
         pygame.display.set_caption("GGLauncher")
-        self.tbimg = pygame.image.load(self.tbimgpath).convert()
-        
+        self.lbimg = pygame.image.load(self.lbimgpath).convert()
+        self.ebimg = pygame.image.load(self.ebimgpath).convert()
+
     def launcherLoop(self):
         launcherUp = True
-        
-        self.background = pygame.Surface((300,500))
+
+        self.background = pygame.Surface((300, 500))
         self.background = self.background.convert()
-        
+
         clock = pygame.time.Clock()
-        
+
         while launcherUp == True:
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     pos = pygame.mouse.get_pos()
                     x = pos[0]
                     y = pos[1]
-                    
+
                     print(x, end=", ")
                     print(y)
-                    
+
                     if (x > 200) & (x < 275) & (y > 350) & (y < 400):
                         launcherUp = False
-                        
+
                     if (x > 200) & (x < 275) & (y > 410) & (y < 460):
                         launcherUp = False
                         sys.exit()
-                        
-            self.background.fill((255,255,255))
-                    
-            
-            self.background.blit(self.tbimg,(200,350))
-            #pygame.draw.rect(self.launcherScreen, (0,0,0), [200, 410, 75, 50])    
-            #pygame.draw.rect(self.background, (255,0,0), [200, 350, 75, 50])
-            
-            
-            self.launcherScreen.blit(self.background,(0,0))
+
+            self.background.fill((255, 255, 255))
+            self.background.blit(self.lbimg, (200, 350))
+            self.background.blit(self.ebimg, (200, 410))
+
+            self.launcherScreen.blit(self.background, (0, 0))
             pygame.display.flip()
-            
+
             clock.tick(20)
-            
