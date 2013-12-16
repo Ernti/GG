49ff1eb7ac01d360010295ed6ed8b41081859fb5
deLordKazz <deLordKazz@LordKazz.fritz.box>
2013-12-16 23:26:53 +0100

    don't know

--- gg/GGlauncher.py
+++ gg/GGlauncher.py
@@ -17,17 +17,18 @@ class GGlauncher():
     def __init__(self):
         self.launcherScreen = pygame.display.set_mode((300,500), pygame.NOFRAME)
         pygame.display.set_caption("GGLauncher")
+        
         self.lbimg = pygame.image.load(self.lbimgpath).convert()
         self.ebimg = pygame.image.load(self.ebimgpath).convert()
         
     def launcherLoop(self):
-        launcherUp = True
         
         self.background = pygame.Surface((300,500))
         self.background = self.background.convert()
         
         clock = pygame.time.Clock()
         
+        launcherUp = True
         while launcherUp == True:
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONDOWN:
@@ -44,8 +45,9 @@ class GGlauncher():
                     if (x > 200) & (x < 275) & (y > 410) & (y < 460):
                         launcherUp = False
                         sys.exit()
+            
                         
-            self.background.fill((255,255,255))     
+            self.background.fill((255,0,0))     
             self.background.blit(self.lbimg,(200,350))
             self.background.blit(self.ebimg,(200,410))
                
@@ -53,4 +55,3 @@ class GGlauncher():
             pygame.display.flip()
             
             clock.tick(20)
-            
